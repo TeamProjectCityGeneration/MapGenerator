9cb53ee4d4c233fd56dc88aaa679d258607c555e
AndrzejFFGrabowski <Andrzej.f.f.Grabowski@gmail.com>
2023-03-28 12:11:23 +0200

    0.3 inequality

--- Procedural_City_Generation/voronoi.py
+++ Procedural_City_Generation/voronoi.py
@@ -11,10 +11,10 @@ def __generate_voronoi():
 
     :return: SciPy voronoi diagram
     """
+    denstity = 9900
+    point_arr = numpy.zeros([denstity, 2], numpy.uint16)
 
-    point_arr = numpy.zeros([900, 2], numpy.uint16)
-
-    for i in range(900):
+    for i in range(denstity):
         point_arr[i][0] = numpy.uint16(random.randint(0, 1600))
         point_arr[i][1] = numpy.uint16(random.randint(0, 900))
 
@@ -35,8 +35,8 @@ def draw_voronoi(pygame_surface,height):
             end_pos = vor.vertices[indx_pair[1]]
             #color = pygame_surface.get_at((int(start_pos[0]),int(start_pos[1])))
             #print(" cordinates ",end_pos[0]," ",end_pos[1]," es ",start_pos[0]," ",start_pos[1]," ")
-            sp = (start_pos)/3
-            ep = (end_pos)/3
+            sp = (start_pos)
+            ep = (end_pos)
             if(checkObsticle(height,ep,sp,h,w)):
                 pygame.draw.line(pygame_surface, (0, 0, 0), ep, sp)
 
@@ -44,8 +44,21 @@ def draw_voronoi(pygame_surface,height):
 
 
 def checkObsticle(height,end_pos,start_pos,h,w):
-    if(abs(end_pos[0])>w or abs(end_pos[1])>h or abs(start_pos[0])>w or abs(start_pos[1])>h):
+    if(abs(end_pos[0])>=w or abs(end_pos[1])>=h or abs(start_pos[0])>=w or abs(start_pos[1])>=h):
+        return False
+    M = [(1,0)]
+    W = [(-1,100)]
+    if(inequality(start_pos,M,W)==False or inequality(end_pos,M,W)==False):
         return False
     if(height[int(start_pos[0]/3),int(start_pos[1]/3)]>=0.4 and height[int(end_pos[0]/3),int(end_pos[1]/3)]>=0.4 and height[int(start_pos[0]/3),int(start_pos[1]/3)]<=0.8 and height[int(end_pos[0]/3),int(end_pos[1]/3)]<=0.8):
         return True
     return False
+
+def inequality(point,linearBigger,linearSmaller):
+    for i in linearBigger:
+        if(point[1] > point[0]*i[0]+i[1]):
+           return False
+    for i in linearSmaller:
+        if(point[1] < point[0]*i[0]+i[1]):
+           return False
+    return True
